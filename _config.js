1613531f1b632dbb0ad62bfb41a567d36ad53767
Isaac <eartharoid@gmail.com>
2020-04-15 17:06:28 +0100

    Update _config.js

--- _config.js
+++ _config.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "Left4Craft", // your server's name
-    token: "TOKEN FROM DISCORDAPP", // do NOT share
+    token: ".......................", // do NOT share
     prefix: "!", // prefix commands with this
     colour: "#4CAF50", // for embeds
     ip: "mc.left4craft.org",
@@ -8,7 +8,7 @@ module.exports = {
     admin_roles: ['424868133967364096', '424868296886583316'],
     activities: ["Minecraft", "on mc.left4craft.org", "with nuclear launch codes", "Minecraft on mc.left4craft.org"],
     activity_type: "PLAYING",
-    cooldown: 3,
+    cooldown: 3, // default (in seconds), can be different for each command
     debug_level: 0,
     log_dm: true,
     log_general: true,
@@ -16,5 +16,27 @@ module.exports = {
     status_cat_id: "697518406013812837",
     poll_chan_id: "697485686688710786",
     chat_bridge_chan_id: "424870757860900865",
-    status_update_interval: 60 // in seconds
+    status_update_interval: 120, // in seconds
+    links: [
+        {
+            name: "Home / Forums",
+            url: "https://www.left4craft.org",
+            pretty: "www.left4craft.org"
+        },
+        {
+            name: "Discord Server",
+            url: "https://discord.left4craft.org",
+            pretty: "discord.left4craft.org"
+        },
+        {
+            name: "Shop / Donation Store",
+            url: "https://www.left4craft.org/shop",
+            pretty: "www.left4craft.org/shop"
+        },
+        {
+            name: "Punishments",
+            url: "https://www.left4craft.org/bans",
+            pretty: "www.left4craft.org/bans"
+        }
+    ] 
 };
