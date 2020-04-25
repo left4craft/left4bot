d9728955c943e9e812b33dd70c452bb3f5f7c159
Isaac <eartharoid@gmail.com>
2020-04-25 21:58:13 +0100

    Update _config.js

--- _config.js
+++ _config.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: "Left4Craft", // your server's name
-    token: ".......................", // do NOT share
+    token: "...............................", // do NOT share
     prefix: "!", // prefix commands with this
     colour: "#4CAF50", // for embeds
     ip: "mc.left4craft.org",
-    query_cache_bypass: ["mc.left4craft.org", "web.left4craft.org", "149.56.107.174"],
+    port: 25565,
     admin_roles: ['424868133967364096', '424868296886583316'],
     activities: ["Minecraft", "on mc.left4craft.org", "with nuclear launch codes", "Minecraft on mc.left4craft.org"],
     activity_type: "PLAYING",
@@ -16,7 +16,7 @@ module.exports = {
     status_cat_id: "697518406013812837",
     poll_chan_id: "697485686688710786",
     chat_bridge_chan_id: "424870757860900865",
-    status_update_interval: 120, // in seconds
+    status_update_interval: 60, // in seconds
     links: [
         {
             name: "Home / Forums",
@@ -37,6 +37,21 @@ module.exports = {
             name: "Punishments",
             url: "https://www.left4craft.org/bans",
             pretty: "www.left4craft.org/bans"
+        },
+        {
+            name: "Status",
+            url: "https://status.left4craft.org",
+            pretty: "status.left4craft.org"
         }
-    ] 
+    ],
+    status_page: "https://status.left4craft.org",
+    status_page_pretty: "status.left4craft.org",
+    subscription_roles: {
+        status: {
+            title: 'Status notifications',
+            description: 'Get mentioned when there is a status notification posted to <#701035082240229477>.\nTo prevent you from throwing your phone at a wall in the event that multiple servers go offline simultaneously, the notification service is limited to **1 ping per 5 minutes**.',
+            role: '701904205144653886',
+            id: 'status'
+        }
+    }
 };
