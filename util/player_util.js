a4860b6517fc8594ee042a694cc6879228d969bd
CaptnSisko <trevor8568@gmail.com>
2020-06-26 13:57:52 -0400

    .

--- util/player_util.js
+++ util/player_util.js
@@ -35,7 +35,7 @@ exports.get_uuid = (input, sql_pool, log, callback) => {
                 sql_pool.query('SELECT uuid FROM discord_users WHERE discordID = ?', [isNaN(input) ? 0 : BigInt(input)], (err, res) => {
                     if(err) log.error(err);
                     if(res[0] !== undefined) {
-                        let uuid = String(res[0]);
+                        let uuid = res[0];
                         uuid = uuid.slice(0,8) + '-' + uuid.slice(8, 13) + '-' + uuid.slice(13, 18) + '-' + uuid.slice(18, 23) + '-' + uuid.slice(23);
                         callback(uuid);
                         return;
