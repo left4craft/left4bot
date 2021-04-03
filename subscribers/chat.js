b399a4d8b1ee28f2d5e6bd02b9b0c3cdd6a134b0
Isaac <git@eartharoid.me>
2021-04-03 23:45:26 +0100

    yes?

--- subscribers/chat.js
+++ subscribers/chat.js
@@ -20,7 +20,7 @@ module.exports = {
 			if (!content) return '';
 			return content
 				.replace(/@(everyone|here)/gmi, '@\u200b$1')
-				.replace(/§|&[0-9A-FK-ORa-fk-or]/g, '');
+				.replace(/(§|&)[0-9A-FK-ORa-fk-or]/g, '');
 		};
 
 		try {
